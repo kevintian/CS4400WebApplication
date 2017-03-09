d42dcbd127eb7c91f739267740a506460fa269b0
kevintian <kevintian@gatech.edu>
2017-03-09 01:06:33 -0500

    Fixed issue with floating labels not activating with jQuery val(), updated calendar to trigger on form click

--- js/datapoint.js
+++ js/datapoint.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     var picker = new MaterialDatetimePicker({})
         .on('submit', function(d) {
+            $("#datetimeLabel").addClass('active');
             $('#datetime').val(d);
         });
     var el = document.querySelector('.c-datepicker-btn');
