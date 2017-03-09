691b0e616d2f4fcc62c75295cff06db52fd2f66e
kevitian <kevin.tian@cox.com>
2017-03-09 15:20:15 -0500

    Added filler pages for all screens, connected pages together

--- js/homepage.js
+++ js/homepage.js
@@ -9,4 +9,20 @@ $(document).ready(function() {
     $('#datacard').on('click', function(event) {
         window.location = "addDataPoint.html";
     });
+
+    $('#pendingDataCard').on('click', function(event) {
+        window.location = "viewPendingData.html";
+    });
+
+    $('#pendingUserCard').on('click', function(event) {
+        window.location = "viewPendingUsers.html";
+    });
+
+    $('#generateReportsCard').on('click', function(event) {
+        window.location = "generatePOIReport.html";
+    });
+
+    $('#filterSearchPOICard').on('click', function(event) {
+        window.location = "filterSearchPOI.html";
+    });
 });
