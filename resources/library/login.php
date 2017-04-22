205bfab0926e213b4862b0f8790675f4b562d9fd
kevintian <kevintian@gatech.edu>
2017-04-22 15:35:10 -0400

    Rejected and pending city officials no longer able to log in

--- resources/library/login.php
+++ resources/library/login.php
@@ -20,12 +20,37 @@ if ($response->num_rows != 0) {
     //Get the users hashed password
     $row = mysqli_fetch_assoc($response); //Gets the first (and only) row as an associative array
 
+    $userType = $row["type"];
     $hashedPass = $row["password"];
     if (password_verify($password, $hashedPass)) {
-        $_SESSION['username'] = $username;
-        $_SESSION['user_type'] = $row["type"];
-        $userInfo = array("user_type" => $_SESSION['user_type'], "username" => $username);
-        echo json_encode($userInfo);
+        //If the password is correct, we check if the account is an accepted city official
+        if ($row["type"] == "City Official") {
+            $stmt = $dbc->prepare('SELECT * FROM CITY_OFFICIAL WHERE username = ?');
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $response = $stmt->get_result();
+
+            $row = mysqli_fetch_assoc($response);
+            $status = $row["approved"];
+
+            if (is_null($status)) {
+                echo "Your account is currently pending.";
+            } else if ($status == 1) {
+                $_SESSION['username'] = $username;
+                $_SESSION['user_type'] = $userType;
+                $userInfo = array("user_type" => $userType, "username" => $username);
+                echo json_encode($userInfo);
+            } else {
+                echo "Your account has been rejected.";
+            }
+
+        } else {
+            //Log in normally if you are not a city official
+            $_SESSION['username'] = $username;
+            $_SESSION['user_type'] = $userType;
+            $userInfo = array("user_type" => $userType, "username" => $username);
+            echo json_encode($userInfo);
+        }
     } else {
         echo "Incorrect username and password combination";
     }
